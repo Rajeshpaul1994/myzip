346c2155d1a1ea54a42c69d225b2c604312f2566
Rajeshpaul1994 <rajeshkumarpaul94@gmail.com>
2022-10-25 18:46:56 +0530

    today live

--- src/components/Home.js
+++ src/components/Home.js
@@ -150,19 +150,25 @@ const makezip = (files) =>{
           </div>
           <div className='mt-3 mb-3 text-white text-center'>{length_file_array} files selected.</div>
           <div className='text-center mt-3 mb-5'>
-            <button className='btn btn-primary me-3 ps-5 pe-5' disabled={downloadBtnDisabled} onClick={() => makezip(selectedFile)}>{downloadBtnTxt}<span className={showLoader ? null : 'd-none'}><span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span></span></button>
-            <button className='btn btn-primary ps-5 pe-5' onClick={() => resetAll()}>Reset</button>
+            <button className='btn btn-warning me-3 ps-5 pe-5' disabled={downloadBtnDisabled} onClick={() => makezip(selectedFile)}>{downloadBtnTxt}<span className={showLoader ? null : 'd-none'}><span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span></span></button>
+            <button className='btn btn-warning ps-5 pe-5' onClick={() => resetAll()}>Reset</button>
           </div>
         </div>
         <div className='col-sm col-lg-8'>
           <div className={selectedFileList ? 'd-none': null }>
             <p className='fs-1 bold text-white'>Fastest online zipping tool..</p>
             <p className='fs-4 text-white'>
-              MyZip is totally free online zip tool, you can create multiple files and folders zip from here within a seconds. But now limitation is you can only zip here upto 2gb of total folders or files. We continuously working on it to solve the limitations. 
+              MyZip is totally free online zip tool, you can create multiple files and folders zip from here within a seconds. But now limitation is you can only zip here upto 2gb of total folders or files. We continuously working on it to add more free tools. 
             </p>
-            <p className='text-white fs-4'>
-            Connect Me:<a href='https://www.linkedin.com/in/rajesh-kumar-paul-python-developer/' ><i className=" ms-3 text-white fs-4 bi bi-linkedin"></i></a>
+            <p className='fs-4 text-white'>
+              <span className='fs-2'>Our upcoming tools..</span> <br/><i className="bi bi-check-circle"></i> Unzip tools <br/><i className="bi bi-check-circle"></i> Remove image background tool 
+              <br/><i className="bi bi-check-circle"></i> SMTP check tool and many more tools. 
             </p>
+            <div>
+              <a className='text-white me-3' href='https://github.com/Rajeshpaul1994'><i className="bi bi-github fs-3"></i></a>
+              <a className='text-white me-3' href='https://www.linkedin.com/in/rajesh-kumar-paul-python-developer/'><i className="bi bi-linkedin fs-3"></i></a>
+              <a className='text-white me-3' href='https://twitter.com/rajesh_myzip'><i className="bi bi-twitter fs-3"></i></a>
+            </div>
           </div>
           <div id='style-4' style={{'height':'500px'}} className={selectedFileList ? 'overflow-auto' : 'd-none'}>
           <table className="table table-striped table-primary rounded-3">
@@ -180,10 +186,10 @@ const makezip = (files) =>{
                 return (
                   <tr key={index}>
                     
-                    <td ><i class="bi bi-file-earmark"></i> {listValue.name}</td>
+                    <td ><i className="bi bi-file-earmark"></i> {listValue.name}</td>
                     
                     <td >{listValue.webkitRelativePath}</td>
-                    <td><button className='bg-transparent cross-btn' onClick={()=>removeListItem(index)}><i  class="bi bi-x-circle"></i></button></td>
+                    <td><button className='bg-transparent cross-btn' onClick={()=>removeListItem(index)}><i  className="bi bi-x-circle"></i></button></td>
                   </tr>
                 );
               })}
