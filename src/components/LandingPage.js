aac0f825879b162a403417be608647b00efe500f
Rajeshpaul1994 <rajeshkumarpaul94@gmail.com>
2022-10-26 10:52:46 +0530

    some changes done

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,6 +4,9 @@ import {Routes, Route, useNavigate} from 'react-router-dom';
 export default function LandingPage() {
   const navigate = useNavigate();
   const navigateToContacts = () => {
+    navigate('/contact');
+  };
+  const navigateToZip = () => {
     navigate('/zip-tool');
   };
   return (
@@ -16,10 +19,10 @@ export default function LandingPage() {
             <div className='col-lg-8 mt-3'>
               <p className='fs-2 bold ubuntu f-700 text-white text-center mt-5'>The free, fun and smart way to create zip<br/> online..</p>
               <div className='mt-3 text-center'>
-                <button className='bg-white mybtn-getstrted' onClick={navigateToContacts}>GET STARTED</button>
+                <button className='bg-white mybtn-getstrted' onClick={navigateToZip}>GET STARTED</button>
               </div>
               <div className='mt-3 text-center'>
-                <button className='bg-white mybtn-getstrted'>I ALREADY HAVE AN ACCOUNT</button>
+                <button className='bg-white mybtn-getstrted' onClick={navigateToContacts}>CONTACT TEAM</button>
               </div>
             </div>
         </div>
